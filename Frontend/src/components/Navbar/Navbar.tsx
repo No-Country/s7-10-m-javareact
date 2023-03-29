7f4eb2c3531e98d14107e9daecf059d5d3d4a04e
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-03-29 07:38:16 -0300

    navbar component updated, new sections available, now it can be visible on layout component

--- Frontend/src/components/Navbar/Navbar.tsx
+++ Frontend/src/components/Navbar/Navbar.tsx
@@ -4,10 +4,10 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false }
+  { name: "Home", href: "#", current: true },
+  { name: "Categories", href: "#", current: false },
+  { name: "FAQ", href: "#", current: false },
+  { name: "Search", href: "#", current: false }
 ];
 
 interface Props {}
