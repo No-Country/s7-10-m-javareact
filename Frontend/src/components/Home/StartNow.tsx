fb1ed92df50aa1157ae894bc3cb2496bf84781aa
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-04-03 18:06:58 -0300

    startNow component started, base text added

---
+++ Frontend/src/components/Home/StartNow.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type Props = {};
+
+const StartNow = (props: Props) => {
+  return (
+    <>
+      <div className="py-6">
+        <div className="justify-start px-4 py-4">
+          <p className="text-xl">
+            You can trust that you're getting the best possible service from the most qualified
+            professionals.{" "}
+          </p>
+        </div>
+        <div className="justify-start px-4 py-4">
+          <p className="text-xl">
+            Plus, we make it easy to get started - simply create your account and connect with
+            professionals who are available and ready to help you.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StartNow;
--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import { BiUserCircle } from "react-icons/bi";
 import { Table } from "../../components/Home/Table";
 import { Header } from "../../components/Home/Header";
+import StartNow from "../../components/Home/StartNow";
 
 export default function Home() {
   return (
     <div className="sm:container m-auto sm:p-12">
       <Header />
       <Table />
+      <StartNow />
     </div>
   );
 }
