86ce21501b9e8ee1f031f01d68055468d5023214
CarolinaCamilaQuevedo <CarolinaCQ>
2023-04-03 14:16:05 -0300

    feat: add config security and jwt filter and service

---
+++ Backend/server/src/main/java/s710m/noCountry/server/security/config/AppSecurity.java
@@ -0,0 +1,48 @@
+package s710m.noCountry.server.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import s710m.noCountry.server.security.filter.JWTFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class AppSecurity {
+
+    private final JWTFilter jwtFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final AccessDeniedHandler accessDeniedHandler;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers("/auth/login", "/auth/register-client", "/auth/register-service-provider")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().exceptionHandling().accessDeniedHandler(accessDeniedHandler).authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
