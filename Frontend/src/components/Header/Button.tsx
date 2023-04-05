9f8d3a62475eab03bce462378d4400893606a593
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-04-05 13:58:06 -0300

    modal finished

---
+++ Frontend/src/components/Header/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ButtonProps {
+  onClick: () => void;
+  children: JSX.Element;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  return (
+    <button className='w-1/8 absolute inset-y-0 right-0 flex items-center sm:hidden pr-2 text-[#FFF]' onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
