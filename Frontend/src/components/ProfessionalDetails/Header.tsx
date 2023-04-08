a85c9bbb356a32df042583dcb13d323474a59f58
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-04-08 17:30:51 -0300

    new data for testing added

--- Frontend/src/components/ProfessionalDetails/Header.tsx
+++ Frontend/src/components/ProfessionalDetails/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import data from "./data.json";
+import { GrFacebook, GrInstagram } from "react-icons/gr";
 
 const Header = () => {
   return (
@@ -7,33 +8,44 @@ const Header = () => {
       <div>
         <div>
           <div className="row mx-3">
-            <div className="col border-red-400 border border-dashed">
-              <div className="absolute">img</div>
-              <div className="row flex justify-around py-4">
-                <div className="col">{data.name}</div>
-                <div className="col">{data.stars}</div>
-                <div className="col">icons</div>
+            <div className="relative col border-red-400 border border-dashed">
+              {/* <div className="absolute">
+                <img src={data.picture} className="w-24 h-24 justify-start" />
+              </div> */}
+              <div className="row flex justify-around py-4 mx-4">
+                <div className="col">
+                  <h2>{data.name}</h2>
+                </div>
+                <div className="col">
+                  <h2>{data.stars}</h2>
+                </div>
+                <div className="col"></div>
               </div>
               <div className="row flex justify-around">
                 <div className="col">
                   <ol className="list-disc">
-                    <li>Plumber</li>
+                    <li>{data.serviceCategories[0].role}</li>
                   </ol>
                 </div>
                 <div className="col">
                   <ol className="list-disc">
-                    <li>Buenos Aires</li>
+                    <li>{data.state}</li>
                   </ol>
                 </div>
               </div>
-              <div className="row flex justify-around">
+              <div className="row flex justify-around py-4 mx-4">
                 <div className="col">
                   <ol className="list-disc">
-                    <li>12 years experience</li>
+                    <li>{data.experience} experience</li>
                   </ol>
                 </div>
-                <div className="col">
-                  <p>icons</p>
+                <div className="col flex py-4 mx-4 justify-between">
+                  <span>
+                    <GrFacebook className="" />
+                  </span>
+                  <span>
+                    <GrInstagram />
+                  </span>
                 </div>
               </div>
             </div>
