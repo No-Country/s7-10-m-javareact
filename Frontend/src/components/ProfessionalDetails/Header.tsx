e82e2e065fe964bb85a73286defe7e1219635eb4
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-04-08 13:20:53 -0300

    stylization and sintaxis updated, code updated with basic data implemented

--- Frontend/src/components/ProfessionalDetails/Header.tsx
+++ Frontend/src/components/ProfessionalDetails/Header.tsx
@@ -1,23 +1,46 @@
 import React from "react";
+import data from "./data.json";
 
 const Header = () => {
   return (
     <>
       <div>
         <div>
-          <div>img</div>
-          <div>
-            <h2>Jack</h2>
-            <p>stars</p>
-            <p>icon</p>
+          <div className="row mx-3">
+            <div className="col border-red-400 border border-dashed">
+              <div className="absolute">img</div>
+              <div className="row flex justify-around py-4">
+                <div className="col">{data.name}</div>
+                <div className="col">{data.stars}</div>
+                <div className="col">icons</div>
+              </div>
+              <div className="row flex justify-around">
+                <div className="col">
+                  <ol className="list-disc">
+                    <li>Plumber</li>
+                  </ol>
+                </div>
+                <div className="col">
+                  <ol className="list-disc">
+                    <li>Buenos Aires</li>
+                  </ol>
+                </div>
+              </div>
+              <div className="row flex justify-around">
+                <div className="col">
+                  <ol className="list-disc">
+                    <li>12 years experience</li>
+                  </ol>
+                </div>
+                <div className="col">
+                  <p>icons</p>
+                </div>
+              </div>
+            </div>
+            <div className="row border-red-400 border-dashed border">
+              <span className="">button</span>
+            </div>
           </div>
-        </div>
-        //tags
-        <div>
-          <ul>
-            <li>tags</li>
-          </ul>
-          <p>icon</p>
         </div>
       </div>
     </>
