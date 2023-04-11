ca9a9f7077edaf71de2448453eeaf8c2b1479554
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-04-11 12:18:31 -0300

    gallery updated, now its finished

--- Frontend/src/components/ProfessionalDetails/Gallery.tsx
+++ Frontend/src/components/ProfessionalDetails/Gallery.tsx
@@ -20,6 +20,10 @@ const Gallery = (props: Props) => {
     setCurrentIndex(newIndex);
   };
 
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
     <div>
       <div className="max-w-[1400px] h-[400px] md:h-[780px]  w-full m-auto py-16 px-4 relative group">
@@ -33,6 +37,17 @@ const Gallery = (props: Props) => {
         {/* Right Arrow */}
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+
+        <div className="flex top4 justify-center py-2">
+          {data.gallery.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className="text-2xl cursor-pointer">
+              <RxDotFilled />
+            </div>
+          ))}
         </div>
       </div>
     </div>
