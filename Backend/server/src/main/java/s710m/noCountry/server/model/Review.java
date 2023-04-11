44cb6827695f6d7961a9d734a633fa6d27fb6282
CarolinaCamilaQuevedo <CarolinaCQ>
2023-04-11 18:46:53 -0300

    feat: add enpoints reviews

--- Backend/server/src/main/java/s710m/noCountry/server/model/Review.java
+++ Backend/server/src/main/java/s710m/noCountry/server/model/Review.java
@@ -1,11 +1,15 @@
 package s710m.noCountry.server.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+
+import java.time.LocalDateTime;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -22,5 +26,23 @@ public class Review {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "service_provider_id")
+    @JsonIgnoreProperties("reviews")
+    private ServiceProvider serviceProvider;
+
+    @CreationTimestamp
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
     
 }
