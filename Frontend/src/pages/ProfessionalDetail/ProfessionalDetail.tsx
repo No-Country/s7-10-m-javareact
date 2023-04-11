bec218ccce8a5a709693ac5d67be2e833a82153f
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-04-11 09:40:08 -0300

    first visit updated

--- Frontend/src/pages/ProfessionalDetail/ProfessionalDetail.tsx
+++ Frontend/src/pages/ProfessionalDetail/ProfessionalDetail.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Header from '../../components/ProfessionalDetails/Header'
-import Descriptions from '../../components/ProfessionalDetails/Descriptions'
+import React from "react";
+import Header from "../../components/ProfessionalDetails/Header";
+import Descriptions from "../../components/ProfessionalDetails/Descriptions";
+import FirstVisit from "../../components/ProfessionalDetails/FirstVisit";
 
-type Props = {}
+type Props = {};
 
 const ProfessionalDetail = (props: Props) => {
   return (
     <div>
-        <Header/>
-        <Descriptions/>
+      <Header />
+      <Descriptions />
+      <FirstVisit />
     </div>
-  )
-}
+  );
+};
 
-export default ProfessionalDetail
+export default ProfessionalDetail;
