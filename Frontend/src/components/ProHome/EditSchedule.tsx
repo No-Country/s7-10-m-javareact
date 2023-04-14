56cc796707d6f668b494c0ed128f5c3ccb045d5d
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-04-14 10:57:20 -0300

    edit schedule and review request on prohomepage updated, now they have stylization and links on it

--- Frontend/src/components/ProHome/EditSchedule.tsx
+++ Frontend/src/components/ProHome/EditSchedule.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BiCalendar } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
-type Props = {}
+type Props = {};
 
 const EditSchedule = (props: Props) => {
   return (
-    <div>EditSchedule</div>
-  )
-}
+    <div>
+      <div className="py-2 w-[300px]">
+        <div className="py-8 flex justify-center align-content-center align-content-center border-[3px] border-solid border-[#FF9A1F] rounded-md">
+          <Link to="/" className="flex place-content-center">
+            <BiCalendar className="text-5xl mr-7" />
+            <span className="mx-4 text-2xl mt-3">Edit Schedule</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EditSchedule
+export default EditSchedule;
